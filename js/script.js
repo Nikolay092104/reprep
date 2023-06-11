d37bfe99993c0724928b0c1e81264873fb29315a
Nikolay092104 <125174836+Nikolay092104@users.noreply.github.com>
2023-06-11 10:57:44 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var navLinks = document.getElementById("navLinks");
+function showMenu(params) {
+    navLinks.style.right = "0";
+}
+function hideMenu(params) {
+    navLinks.style.right = "-200px";
+}
